9529dc68787dc0cd87ce5dc202529e5f0bd3e998
markabiera <markwil.abiera0518@gmail.com>
2021-05-03 18:41:19 +0800

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Fundament Skill Test
+                    Laravel Fundamental Skill Test
                 </div>
 
                
